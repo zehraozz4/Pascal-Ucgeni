f4313ae160df66003fe38446f378ea9dbcd66af9
ZEHRA ÖZ <33zehraoz@gmail.com>
2025-04-28 18:53:29 +0300

    Pascal Ucgeni
    c# ile gelistirilmis pascal ucgeni yazdirma

---
+++ pascalUcgeni/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pascalUcgeni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pasgal ucgeni olusturma uygulamasina hosgeldiniz...");
+            while(true)
+            {
+                Console.WriteLine("\n----------ANA MENU----------");
+                Console.WriteLine("Pascal ucgeni olusturmak icin --> 1");
+                Console.WriteLine("Cikis icin --> 0");
+                Console.Write("Lutfen yapmak istediginiz islemi seciniz:");
+                string secim = Console.ReadLine();
+
+                switch(secim)
+                {
+                    case "0":
+                        Console.WriteLine("Bizi tercih ettiginiz icin tesekkur ederiz. Cikis yapiliyor...");
+                        Environment.Exit(0);
+                        break;
+                    case "1":
+                        Console.Write("Lutfen ucgenin kac satir icerecegini giriniz:");
+                        string stringSatir = Console.ReadLine();
+                        if(int.TryParse(stringSatir,out int satir))
+                        {
+                           int[,] dizi = new int[satir,satir];
+                           int i = 0, j = 0;
+                           for(i=0;i<satir;i++)
+                            {
+                                for(j=0;j<=i;j++)
+                                {
+                                    if(j==0 || j==i)
+                                    {
+                                        dizi[i,j] = 1;
+                                    }
+                                    else
+                                    {
+                                        dizi[i,j] = dizi[i - 1,j - 1] + dizi[i - 1,j];
+                                    }
+                                }
+                            }
+
+                           for(i=0;i<satir; i++)
+                            {
+                                for(int k=0;k<satir-i-1;k++)
+                                {
+                                    Console.Write(" ");
+                                }
+                                for(j=0;j<=i;j++)
+                                {
+                                    Console.Write(dizi[i,j]+" ");
+                                }
+                                Console.WriteLine();
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Lutfen gecerli deger giriniz!!!");
+                            continue;
+                        }
+                            break;
+                    default:
+                        Console.WriteLine("Lutfen gecerli deger giriniz!!!");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ pascalUcgeni/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("pascalUcgeni")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("pascalUcgeni")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("ea6491f8-e570-4a38-8f26-f750f9f1eca6")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
